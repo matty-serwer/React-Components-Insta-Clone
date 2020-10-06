66c0702a9bdf20828b4a3f96b78e4aadc0393087
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-06 16:39:48 -0400

    LikeSection working, but with unlimited likes.

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import Comments from '../Comments/Comments';
-import LikeSection from './LikeSection';
-import PostHeader from './PostHeader';
+import React from "react";
+import Comments from "../Comments/Comments";
+import LikeSection from "./LikeSection";
+import PostHeader from "./PostHeader";
 
-const Post = props => {
+const Post = (props) => {
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
 
   return (
     <div className='post-border'>
-      <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
-      />
+      <PostHeader username={post.username} thumbnailUrl={post.thumbnailUrl} />
       <div className='post-image-wrapper'>
-        <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
-        />
+        <img alt='post thumbnail' className='post-image' src={post.imageUrl} />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection
+        key={post.id}
+        likePost={() => likePost(post.id)}
+        numberOfLikes={post.likes}
+      />
       {/* Comments also wants its props! */}
-      <Comments />
+      {post.comments.map((comment) => {
+        return <Comments key={comment.id} comment={comment} />
+      })}
+      
     </div>
   );
 };
